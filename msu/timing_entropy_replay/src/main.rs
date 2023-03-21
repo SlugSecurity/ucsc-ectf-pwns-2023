ebe3445d5e0b1a09a5d4047459f7b8aa3a21d78a
Victor Ho <vicho@ucsc.edu>
2023-03-20 22:58:38 -0700

    Get it closer to working

--- msu/timing_entropy_replay/src/main.rs
+++ msu/timing_entropy_replay/src/main.rs
@@ -1,12 +1,6 @@
 use clap::Parser;
 use serialport::SerialPort;
-use std::{
-    collections::HashMap,
-    path::PathBuf,
-    process::Command,
-    str, thread,
-    time::{Duration, Instant},
-};
+use std::{collections::HashMap, path::PathBuf, process::Command, str, thread, time::Duration};
 
 #[derive(Parser)]
 struct Args {
@@ -49,33 +43,99 @@ struct Args {
 const BAUD_RATE: u32 = 115200;
 
 const RESET_HOLD_TIME: Duration = Duration::from_micros(20);
-const MODE_CHANGE_TIME: Duration = Duration::from_secs(10);
+const BUTTON_HOLD_TIME: Duration = Duration::from_millis(100);
+
+const CAR_BOARD_NUMBER: u32 = 0;
+const FOB_BOARD_NUMBER: u32 = 1;
 
 const UNLOCK_MESSAGE: [u8; 1] = [0x56];
 const CHALLENGE_LEN: usize = 64;
 const RESPONSE_LEN: usize = 256;
 
+fn reset_board(esp32: &mut Box<dyn SerialPort>, board_number: u32) {
+    esp32
+        .write_all(format!("l{board_number}r\n").as_bytes())
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    thread::sleep(RESET_HOLD_TIME);
+
+    esp32
+        .write_all(format!("h{board_number}r\n").as_bytes())
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+}
+
+fn reset_board_install(esp32: &mut Box<dyn SerialPort>, board_number: u32) {
+    esp32
+        .write_all(format!("l{board_number}2\n").as_bytes())
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    thread::sleep(BUTTON_HOLD_TIME);
+
+    esp32
+        .write_all(format!("l{board_number}r\n").as_bytes())
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    thread::sleep(RESET_HOLD_TIME);
+
+    esp32
+        .write_all(format!("h{board_number}r\n").as_bytes())
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    thread::sleep(BUTTON_HOLD_TIME);
+
+    esp32
+        .write_all(format!("h{board_number}2\n").as_bytes())
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    thread::sleep(Duration::from_secs(1));
+}
+
 fn do_mode_change(esp32: &mut Box<dyn SerialPort>, dev1_serial: &str, dev2_serial: &str) {
-    // Reset both boards while holding down SW1
-    for board_number in [0, 1] {
-        esp32
-            .write_all(format!("l{board_number}r\n").as_bytes())
-            .expect("Failed to write to ESP32.");
-        esp32
-            .write_all(format!("l{board_number}1\n").as_bytes())
-            .expect("Failed to write to ESP32.");
+    esp32
+        .write_all(b"l01\n")
+        .expect("Failed to write to ESP32.");
+    esp32
+        .write_all(b"l11\n")
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
 
-        thread::sleep(RESET_HOLD_TIME);
+    thread::sleep(BUTTON_HOLD_TIME);
 
-        esp32
-            .write_all(format!("h{board_number}r\n").as_bytes())
-            .expect("Failed to write to ESP32.");
-        esp32
-            .write_all(format!("h{board_number}1\n").as_bytes())
-            .expect("Failed to write to ESP32.");
-    }
+    esp32
+        .write_all(b"l0r\n")
+        .expect("Failed to write to ESP32.");
+    esp32
+        .write_all(b"l1r\n")
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
 
-    Command::new("python3")
+    thread::sleep(RESET_HOLD_TIME);
+
+    esp32
+        .write_all(b"h0r\n")
+        .expect("Failed to write to ESP32.");
+    esp32
+        .write_all(b"h1r\n")
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    thread::sleep(BUTTON_HOLD_TIME);
+
+    esp32
+        .write_all(b"h01\n")
+        .expect("Failed to write to ESP32.");
+    esp32
+        .write_all(b"h11\n")
+        .expect("Failed to write to ESP32.");
+    esp32.flush().unwrap();
+
+    let mut child = Command::new("python3")
         .args([
             "-m",
             "ectf_tools",
@@ -85,27 +145,21 @@ fn do_mode_change(esp32: &mut Box<dyn SerialPort>, dev1_serial: &str, dev2_seria
             "--dev2-serial",
             dev2_serial,
         ])
-        .output()
+        .spawn()
         .expect("Failed to do mode change.");
 
+    child.wait().unwrap();
+
     // Reset both boards again
-    for board_number in [0, 1] {
-        esp32
-            .write_all(format!("l{board_number}r\n").as_bytes())
-            .expect("Failed to write to ESP32.");
-
-        thread::sleep(RESET_HOLD_TIME);
-
-        esp32
-            .write_all(format!("h{board_number}r\n").as_bytes())
-            .expect("Failed to write to ESP32.");
-    }
+    reset_board(esp32, FOB_BOARD_NUMBER);
+    reset_board(esp32, CAR_BOARD_NUMBER);
 }
 
 fn main() {
     let args = Args::parse();
 
     let mut car_uart1 = serialport::new(&args.car_uart1_serial_file_name, BAUD_RATE)
+        .timeout(Duration::from_secs(5))
         .open()
         .expect("Failed to open UART1 serial port.");
 
@@ -114,6 +168,7 @@ fn main() {
         .expect("Failed to clear UART1 serial port.");
 
     let mut fob_uart1 = serialport::new(&args.fob_uart1_serial_file_name, BAUD_RATE)
+        .timeout(Duration::from_secs(5))
         .open()
         .expect("Failed to open UART1 serial port.");
 
@@ -125,8 +180,10 @@ fn main() {
         .open()
         .expect("Failed to open ESP32 serial port.");
 
+    reset_board_install(&mut esp32, FOB_BOARD_NUMBER);
+
     // Flash the fob firmware
-    Command::new("python3")
+    let mut child = Command::new("python3")
         .args([
             "-m",
             "ectf_tools",
@@ -138,14 +195,21 @@ fn main() {
             "--dev-serial",
             &args.fob_uart0_serial_file_name,
         ])
-        .output()
+        .spawn()
         .expect("Failed to flash fob.");
+
+    child.wait().unwrap();
+
+    // Reset the fob
+    reset_board(&mut esp32, FOB_BOARD_NUMBER);
 
     let mut responses = HashMap::new();
 
     for _ in 0..args.paired_attempts {
+        reset_board_install(&mut esp32, CAR_BOARD_NUMBER);
+
         // Flash the car firmware
-        Command::new("python3")
+        let mut child = Command::new("python3")
             .args([
                 "-m",
                 "ectf_tools",
@@ -157,54 +221,65 @@ fn main() {
                 "--dev-serial",
                 &args.car_uart0_serial_file_name,
             ])
-            .output()
+            .spawn()
             .expect("Failed to flash car.");
 
+        child.wait().unwrap();
+
         // Reset the car
+        reset_board(&mut esp32, CAR_BOARD_NUMBER);
+
+        thread::sleep(Duration::from_secs(1));
+
+        // Press the fob button
         esp32
-            .write_all(b"l0r\n")
+            .write_all(b"l11\n")
             .expect("Failed to write to ESP32.");
-        thread::sleep(RESET_HOLD_TIME);
+        thread::sleep(BUTTON_HOLD_TIME);
         esp32
-            .write_all(b"h0r\n")
+            .write_all(b"h11\n")
             .expect("Failed to write to ESP32.");
 
-        // Wait some time to account for doing the mode change later
-        thread::sleep(MODE_CHANGE_TIME);
-
-        // Send the unlock message and wait
-        car_uart1
-            .write_all(&UNLOCK_MESSAGE)
-            .expect("Failed to write to UART1.");
-        thread::sleep(Duration::from_millis(100));
-
-        // Receive a challenge from the car
-        let mut challenge = [0u8; CHALLENGE_LEN];
-        match car_uart1.read(&mut challenge) {
-            Ok(CHALLENGE_LEN) => {
-                println!("Received challenge {challenge:?}")
-            }
-            Ok(n) => {
-                panic!("Tried to read {CHALLENGE_LEN} bytes from UART1 but only got {n} bytes.")
-            }
+        let mut request_byte = [0; 1];
+        match fob_uart1.read(&mut request_byte) {
+            Ok(1) => (),
+            Ok(n) => println!("Read {n} bytes"),
             Err(e) => panic!("Failed to read from UART1: {}", e),
         }
 
-        // Send the challenge to the paired fob and wait
-        fob_uart1
-            .write_all(&challenge)
-            .expect("Failed to write to UART1.");
-        thread::sleep(Duration::from_millis(100));
+        let mut challenge_byte = [0; 1];
+        match car_uart1.read(&mut challenge_byte) {
+            Ok(1) => (),
+            Ok(n) => println!("Read {n} bytes"),
+            Err(e) => panic!("Failed to read from UART1: {}", e),
+        }
+
+        // Receive a challenge from the car
+        let mut challenge = [0; CHALLENGE_LEN];
+        match car_uart1.read(&mut challenge) {
+            Ok(CHALLENGE_LEN) => {
+                println!("Received challenge {challenge:?}");
+            },
+            Ok(n) => panic!("Tried to read {CHALLENGE_LEN} bytes from UART1 but only got {n} bytes: {challenge:?}"),
+            Err(e) => panic!("Failed to read from UART1: {}", e),
+        }
+
+        let mut response_byte = [0; 1];
+        match fob_uart1.read(&mut response_byte) {
+            Ok(1) => (),
+            Ok(n) => println!("Read {n} bytes"),
+            Err(e) => panic!("Failed to read from UART1: {}", e),
+        }
 
         // Receive the response from the paired fob
         let mut response = [0; RESPONSE_LEN];
         match fob_uart1.read(&mut response) {
             Ok(RESPONSE_LEN) => {
-                println!("Received response {response:?} for challenge {challenge:?}");
+                println!("Received response {response:?}");
                 responses.insert(challenge, response);
             }
             Ok(n) => {
-                panic!("Tried to read {RESPONSE_LEN} bytes from UART1 but only got {n} bytes.")
+                panic!("Tried to read {RESPONSE_LEN} bytes from UART1 but only got {n} bytes: {response:?}")
             }
             Err(e) => panic!("Failed to read from UART1: {}", e),
         }
@@ -213,8 +288,10 @@ fn main() {
     let mut found = false;
 
     for _ in 0..args.unpaired_attempts {
+        reset_board_install(&mut esp32, CAR_BOARD_NUMBER);
+
         // Flash the car firmware
-        Command::new("python3")
+        let mut child = Command::new("python3")
             .args([
                 "-m",
                 "ectf_tools",
@@ -226,32 +303,43 @@ fn main() {
                 "--dev-serial",
                 &args.car_uart0_serial_file_name,
             ])
-            .output()
+            .spawn()
             .expect("Failed to flash car.");
 
-        // Do the mode change then wait so timing is consistent
-        let mode_change_start = Instant::now();
+        child.wait().unwrap();
+
         do_mode_change(
             &mut esp32,
             &args.car_uart0_serial_file_name,
             &args.fob_uart0_serial_file_name,
         );
-        while mode_change_start.elapsed() < MODE_CHANGE_TIME {}
 
         // Reset the car
-        esp32
-            .write_all(b"l0r\n")
-            .expect("Failed to write to ESP32.");
-        thread::sleep(RESET_HOLD_TIME);
-        esp32
-            .write_all(b"h0r\n")
-            .expect("Failed to write to ESP32.");
+        reset_board(&mut esp32, CAR_BOARD_NUMBER);
+
+        thread::sleep(Duration::from_secs(1));
+
+        car_uart1
+            .write_all(&UNLOCK_MESSAGE)
+            .expect("Failed to write to UART1.");
+        car_uart1.flush().unwrap();
+
+        let mut challenge_byte = [0; 1];
+        match car_uart1.read(&mut challenge_byte) {
+            Ok(1) => (),
+            Ok(_) => println!("hmm"),
+            Err(e) => panic!("Failed to read from UART1: {}", e),
+        }
+        println!("{challenge_byte:?}");
 
         // Receive a challenge from the car
         let mut challenge = [0; CHALLENGE_LEN];
         match car_uart1.read(&mut challenge) {
-            Ok(CHALLENGE_LEN) => (),
+            Ok(CHALLENGE_LEN) => {
+                println!("Got challenge {challenge:?}");
+            }
             Ok(n) => {
+                println!("{challenge:?}");
                 panic!("Tried to read {CHALLENGE_LEN} bytes from UART1 but only got {n} bytes.")
             }
             Err(e) => panic!("Failed to read from UART1: {}", e),
