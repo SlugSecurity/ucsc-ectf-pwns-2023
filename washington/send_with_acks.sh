01404c0aa800cf7efa7db6ef0a20b30a2797ac1f
Victor Ho <vicho@ucsc.edu>
2023-03-30 21:13:21 -0700

    Don't send an ACK if the part of the message isn't long enough

--- washington/send_with_acks.sh
+++ washington/send_with_acks.sh
@@ -5,8 +5,10 @@ FILE="$1"
 BYTES_PER_ACK=8
 ACK_MESSAGE="A"
 
-while read -r -n "$BYTES_PER_ACK" bytes; do
+while read -r -n $BYTES_PER_ACK bytes; do
     echo -n "$bytes"
-    echo -n "$ACK_MESSAGE"
-    sleep 0.01
+    if [[ ${#bytes} -eq $BYTES_PER_ACK ]]; then
+        echo -n "$ACK_MESSAGE"
+        sleep 0.01
+    fi
 done < "$FILE"
