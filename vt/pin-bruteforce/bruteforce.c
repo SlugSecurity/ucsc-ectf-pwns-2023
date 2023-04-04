105c23a8cd2ecb5c23696a92d7261ed13956558c
ubuntu 22.04 vb <jdkohls@ucsc.edu>
2023-04-04 13:49:05 -0700

    bruteforce update to make my life horrible; made the pin a string

--- vt/pin-bruteforce/bruteforce.c
+++ vt/pin-bruteforce/bruteforce.c
@@ -5,7 +5,7 @@
 
 
 //change this to be the pin hash we get from ROP
-#define sha_256_pin "B78C53B964AB04FEFE19BEE2600B38E84A2C4BA735D23BE7600965221E0979B7"
+#define sha_256_pin "f7298948225be27bf643ab6ac049c6bf953ab0d6a9513786a49692941142169b"
 
 #define DIGEST_BYTES 32
 
@@ -23,12 +23,19 @@ int main(void){
     //this should be 16^6+1
     for(;pin < 16777217;pin++){
         memcpy(arr_pin,&pin,sizeof(pin));
+
+        //I hate all of this
+        char string_pin[8];
+        sprintf(string_pin,"%lx",pin);
+
         sha256_ctx cx[1];
         uint8_t message_hash[DIGEST_BYTES];
         size_t len = 6;
 
         sha256_begin(cx);
-        sha256(message_hash, arr_pin, len, cx);
+
+        //if this doesnt work, replace with arr_pin
+        sha256(message_hash, (uint8_t *)string_pin, len, cx);
         
         if (0==(strncmp((char *)message_hash, (char *)received_hash, DIGEST_BYTES))){
             printf("Pin is %lx\n",pin);
